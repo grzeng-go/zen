8148f7129d78723094da97ca3151602caab2e2f7
grzeng-go <grzeng-go@sina.com>
2020-07-30 01:07:23 +0800

    延迟队列测试精度调试 时间轮接口

--- queue/delay_queue_test.go
+++ queue/delay_queue_test.go
@@ -32,13 +32,13 @@ func (item *DelayedEntry) SetIndex(i int) {
 }
 
 func (item *DelayedEntry) GetDelay() time.Duration {
-	return time.Unix(item.Priority, 0).Sub(time.Now())
+	return time.Duration(item.Priority - time.Now().UnixNano())
 }
 
 func newEntry(v string, t time.Duration) Delayed {
 	return &DelayedEntry{
 		Value:    v,
-		Priority: time.Now().Add(t).Unix(),
+		Priority: time.Now().Add(t).UnixNano(),
 	}
 }
 
@@ -48,12 +48,12 @@ func TestDelayQueue(t *testing.T) {
 	dq.Offer(newEntry("2", 2*time.Second))
 	dq.Offer(newEntry("3", 1*time.Second))
 	dq.Offer(newEntry("4", 15*time.Second))
-	t.Logf("time: %v", time.Now())
+	t.Logf("time: %v", time.Now().UnixNano()/1e8)
 	ctx, _ := context.WithTimeout(context.Background(), 17*time.Second)
 	for i := 0; i < 5; i++ {
 		go func(i int) {
 			t.Logf("i: %v; value: %v", i, dq.Take(ctx))
-			t.Logf("time: %v", time.Now())
+			t.Logf("time: %v", time.Now().UnixNano()/1e8)
 		}(i)
 	}
 	time.Sleep(20 * time.Second)
--- timingwheel/timing_wheel.go
+++ timingwheel/timing_wheel.go
@@ -1,1 +1,38 @@
 package timingwheel
+
+import (
+	"github.com/grzeng-go/zen/queue"
+	"time"
+)
+
+type Task interface {
+	// 运行任务
+	Run()
+	// 取消任务
+	Cancel()
+	// 获取任务执行时间纳秒数 time.Now().UnixNano()
+	GetDelay() time.Duration
+}
+
+// 时间轮上的每个桶，时间都是固定的
+type TaskList interface {
+	queue.Delayed
+	// 插入任务
+	Add(t Task)
+	// 移除任务
+	Remove(t Task)
+	// 重新插入
+	Reinsert()
+}
+
+type Timer interface {
+	// 插入任务
+	Add(t Task)
+	// 移除任务
+	Remove(t Task)
+	// 推进时间轮
+	Advance(t time.Duration)
+	// 关闭时间轮
+	// b 用来判断是否要等待最后一个任务结束再关闭
+	Close(b bool)
+}
